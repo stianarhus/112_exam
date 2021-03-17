3315288532d7a276772ed24055fb55e6bd2ce3d5
stianarhus <stian.arhus@student.uib.no>
2021-03-17 10:45:45 +0100

    importert

---
+++ example.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+https://github.com/stianarhus/284_exam
+
+
+https://github.com/git-guides/install-git
